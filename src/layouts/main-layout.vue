3c55a2352ee250dd08102c51434b939e244c1525
Rafael Marques <rafael.marques-98@hotmail.com>
2025-07-05 02:05:55 -0300

    feat: config theme

---
+++ src/layouts/main-layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-layout">
+    <router-view />
+  </div>
+</template>
+
+<style>
+  .page-layout{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+
+    background-color: red;
+  }
+</style>
---
+++ src/pages/main-page.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- <q-layout>
+    <q-page-container>
+
+    </q-page-container>
+  </q-layout> -->
+
+  <div class="page">
+
+  </div>
+</template>
+
+<style>
+  .page{
+    width: 50%;
+    height: 50%;
+
+    background-color: blue;
+  }
+</style>
